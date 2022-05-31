e7a27394aba373bc5b73eaecff86403587ea8d44
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-05-31 16:19:05 +0530

    Added : Starting Position of the Player and Dice

---
+++ src/com/bridgelabz/SnakeLadder.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+
+        int playerPosition = 0;
+        int diceRoll = 0;
+
+        System.out.println("Starting Position of the Player = " + playerPosition);
+
+        double randomnumber = (double) (Math.random() * 6) + 1;
+
+        diceRoll = (int) randomnumber;
+
+        System.out.println("Dice Number =" + " " + diceRoll);
+
+    }
+}
